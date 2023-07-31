5bd421c566afbe1560b0aa7b57504d2f561d8277
Filipe Toscano <filipe.toscano@gmail.com>
2023-07-31 20:52:54 +0400

    Add missing ValidationStatus value, split classes in Domain.cs into own files

---
+++ src/Resend/DomainRecord.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Resend;
+
+/// <summary />
+public class DomainRecord
+{
+    /// <summary />
+    [JsonPropertyName( "record" )]
+    public string Record { get; set; } = default!;
+
+    /// <summary />
+    [JsonPropertyName( "name" )]
+    public string Name { get; set; } = default!;
+
+    /// <summary />
+    [JsonPropertyName( "value" )]
+    [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+    public string? Value { get; set; }
+
+    /// <summary>
+    /// Type of DNS record to be added.
+    /// </summary>
+    [JsonPropertyName( "type" )]
+    public string RecordType { get; set; } = default!;
+
+    /// <summary />
+    [JsonPropertyName( "ttl" )]
+    public string TimeToLive { get; set; } = default!;
+
+    /// <summary />
+    [JsonPropertyName( "priority" )]
+    [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+    public int? Priority { get; set; } = default!;
+
+    /// <summary />
+    [JsonPropertyName( "status" )]
+    public ValidationStatus Status { get; set; }
+}
---
+++ src/Resend/ValidationStatus.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Resend;
+
+/// <summary />
+[JsonConverter( typeof( JsonStringEnumValueConverter<ValidationStatus> ) )]
+public enum ValidationStatus
+{
+    /// <summary>
+    /// Validation has not started.
+    /// </summary>
+    [JsonStringValue( "not_started" )]
+    NotStarted,
+
+    /// <summary>
+    /// Validation has been started and is currently executing.
+    /// </summary>
+    /// <remarks>
+    /// May take up to 72h to conclude.
+    /// </remarks>
+    [JsonStringValue( "pending" )]
+    Pending,
+
+    /// <summary>
+    /// Validation has failed.
+    /// </summary>
+    [JsonStringValue( "failed" )]
+    Failed,
+}
