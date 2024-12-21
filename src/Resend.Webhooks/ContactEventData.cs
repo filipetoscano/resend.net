7caba9f38b7885d3beb164063de0950d03019f6f
Filipe Toscano <filipe.toscano@gmail.com>
2024-12-21 17:47:03 +0400

    wip: Webhook implementation
    Related to #8

--- src/Resend.Webhooks/ContactEventData.cs
+++ src/Resend.Webhooks/ContactEventData.cs
@@ -8,4 +8,34 @@ public class ContactEventData : IWebhookData
     /// <summary />
     [JsonPropertyName( "id" )]
     public Guid ContactId { get; set; }
+
+    /// <summary />
+    [JsonPropertyName( "audience_id" )]
+    public Guid AudienceId { get; set; }
+
+    /// <summary />
+    [JsonPropertyName( "created_at" )]
+    [JsonConverter( typeof( JsonUtcDateTimeConverter ) )]
+    public DateTime MomentCreated { get; set; }
+
+    /// <summary />
+    [JsonPropertyName( "updated_at" )]
+    [JsonConverter( typeof( JsonUtcDateTimeConverter ) )]
+    public DateTime MomentUpdated { get; set; }
+
+    /// <summary />
+    [JsonPropertyName( "email" )]
+    public string Email { get; set; } = default!;
+
+    /// <summary />
+    [JsonPropertyName( "first_name" )]
+    public string FirstName { get; set; } = default!;
+
+    /// <summary />
+    [JsonPropertyName( "last_name" )]
+    public string LastName { get; set; } = default!;
+
+    /// <summary />
+    [JsonPropertyName( "unsubscribed" )]
+    public bool IsUnsubscribe { get; set; }
 }
--- src/Resend.Webhooks/WebhookValidator.cs
+++ src/Resend.Webhooks/WebhookValidator.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Svix.Exceptions;
 using System.Net;
+using System.Text.Json;
 
 namespace Resend.Webhooks;
 
@@ -18,7 +21,38 @@ public class WebhookValidator
 
 
     /// <summary />
-    public bool IsValid( string payload, WebHeaderCollection headers )
+    public ActionResult<Webhook> Validate( IHeaderDictionary header, string payload )
+    {
+        /*
+         * TODO:
+         */
+        var h = new WebHeaderCollection();
+        h.Add( "svix-id", "" );
+        h.Add( "svix-timestamp", "" );
+        h.Add( "svix-signature", "" );
+
+
+        /*
+         * 
+         */
+        if ( IsValid( h, payload ) == false )
+            return new BadRequestResult();
+
+
+        /*
+         * 
+         */
+        var obj = JsonSerializer.Deserialize<Webhook>( payload );
+
+        if ( obj == null )
+            return new BadRequestResult();
+
+        return obj;
+    }
+
+
+    /// <summary />
+    public bool IsValid( WebHeaderCollection headers, string payload )
     {
         /*
          * 
