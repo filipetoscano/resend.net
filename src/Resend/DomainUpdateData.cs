d7319a9e9ee0930d3a258061949c7713fb68de6d
Filipe Toscano <filipe.toscano@gmail.com>
2024-12-30 14:33:39 +0400

    Add tls property to domain update
    Related to #13

--- src/Resend/DomainUpdateData.cs
+++ src/Resend/DomainUpdateData.cs
@@ -16,4 +16,34 @@ public class DomainUpdateData
     /// </summary>
     [JsonPropertyName( "open_tracking" )]
     public bool TrackOpen { get; set; } = default!;
+
+    /// <summary>
+    /// TLS mode.
+    /// </summary>
+    [JsonPropertyName( "tls" )]
+    [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+    public TlsMode? TlsMode { get; set; }
+}
+
+
+/// <summary />
+[JsonConverter( typeof( JsonStringEnumValueConverter<TlsMode> ) )]
+public enum TlsMode
+{
+    /// <summary>
+    /// Opportunistic TLS means that it always attempts to make a
+    /// secure connection to the receiving mail server. If it can't
+    /// establish a secure connection, it sends the message unencrypted.
+    /// </summary>
+    [JsonStringValue( "opportunistic" )]
+    Opportunistic = 1,
+
+    /// <summary>
+    /// Enforced TLS on the other hand, requires that the email
+    /// communication must use TLS no matter what. If the
+    /// receiving server does not support TLS, the email will
+    /// not be sent.
+    /// </summary>
+    [JsonStringValue( "enforced" )]
+    Enforced,
 }
