filipetoscano/resend.net

commit 7a041cf29d4ef08f0788d0b015fb64ca769e496e

Remove not implemented webhook commands
They aren't implementable, since the public API has no methods for webhooks.

tools/Resend.Cli/WebhookCommand.cs
@@ -4,10 +4,8 @@ namespace Resend.Cli;
 
 /// <summary />
 [Command( "webhook", Description = "Webhook management" )]
-[Subcommand( typeof( Webhook.WebhookCreateCommand ))]
-[Subcommand( typeof( Webhook.WebhookListCommand ))]
-[Subcommand( typeof( Webhook.WebhookDeleteCommand ) )]
 [Subcommand( typeof( Webhook.WebhookPostCommand ) )]
+public class WebhookCommand
 public class WebhookCommand
 {
     /// <summary />

tools/Resend.Cli/Webhook/WebhookCreateCommand.cs
@@ -1,24 +1,0 @@
-﻿using McMaster.Extensions.CommandLineUtils;
-
-namespace Resend.Cli.Webhook;
-
-/// <summary />
-[Command( "create" )]
-public class WebhookCreateCommand
-{
-    private readonly IResend _resend;
-
-
-    /// <summary />
-    public WebhookCreateCommand( IResend resend )
-    {
-        _resend = resend;
-    }
-
-
-    /// <summary />
-    public Task<int> OnExecuteAsync()
-    {
-        throw new NotImplementedException();
-    }
-}

tools/Resend.Cli/Webhook/WebhookDeleteCommand.cs
@@ -1,35 +1,0 @@
-﻿using McMaster.Extensions.CommandLineUtils;
-using System.ComponentModel.DataAnnotations;
-
-namespace Resend.Cli.Webhook;
-
-/// <summary />
-[Command( "delete" )]
-public class WebhookDeleteCommand
-{
-    private readonly IResend _resend;
-
-
-    /// <summary />
-    [Argument( 0, Description = "API key identifier" )]
-    [Required]
-    public Guid WebhookId { get; set; }
-
-
-    /// <summary />
-    public WebhookDeleteCommand( IResend resend )
-    {
-        _resend = resend;
-    }
-
-
-    /// <summary />
-    public async Task<int> OnExecuteAsync()
-    {
-        await Task.Delay( 100 );
-
-        // await _resend.WebhookDelete( this.KeyId );
-
-        return 0;
-    }
-}

tools/Resend.Cli/Webhook/WebhookListCommand.cs
@@ -1,65 +1,0 @@
-﻿using McMaster.Extensions.CommandLineUtils;
-using Spectre.Console;
-using System.Text.Json;
-
-namespace Resend.Cli.Webhook;
-
-/// <summary />
-[Command( "list" )]
-public class WebhookListCommand
-{
-    private readonly IResend _resend;
-
-
-    /// <summary />
-    [Option( "-j|--json", CommandOptionType.NoValue, Description = "Emit output as JSON array" )]
-    public bool InJson { get; set; }
-
-
-    /// <summary />
-    public WebhookListCommand( IResend resend )
-    {
-        _resend = resend;
-    }
-
-
-    /// <summary />
-    public async Task<int> OnExecuteAsync()
-    {
-        //var res = await _resend.WebhookListAsync();
-        await Task.Delay( 0 );
-        var hooks = new List<Resend.Webhook>();
-
-
-        if ( this.InJson == true )
-        {
-            var jso = new JsonSerializerOptions() { WriteIndented = true };
-            var json = JsonSerializer.Serialize( hooks, jso );
-
-            Console.WriteLine( json );
-        }
-        else
-        {
-            var table = new Table();
-            table.Border = TableBorder.SimpleHeavy;
-            table.AddColumn( "Webhook Id" );
-            table.AddColumn( "Endpoint URL" );
-            table.AddColumn( "Events" );
-            table.AddColumn( "Created" );
-
-            foreach ( var d in hooks )
-            {
-                table.AddRow(
-                    new Markup( d.Id.ToString() ),
-                    new Markup( d.EndpointUrl ),
-                    new Markup( string.Join( ",", d.Events ) ),
-                    new Markup( d.MomentCreated.ToString() )
-                );
-            }
-
-            AnsiConsole.Write( table );
-        }
-
-        return 0;
-    }
-}

tools/Resend.Cli/Webhook/WebhookPostCommand.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Resend.Cli.Webhook;
 
 /// <summary />
-[Command( "post" )]
+[Command( "post", Description = "Post a webhook event onto a Webhook sink" )]
 public class WebhookPostCommand
 {
     /// <summary />

commit 21e96fabd1d2d700f34e0da9b4516d7e266b691a

Fix broken build
Broken in 7a041cf29d4ef08f0788d0b015fb64ca769e496e

tools/Resend.Cli/WebhookCommand.cs
@@ -6,7 +6,6 @@ namespace Resend.Cli;
 [Command( "webhook", Description = "Webhook management" )]
 [Subcommand( typeof( Webhook.WebhookPostCommand ) )]
 public class WebhookCommand
-public class WebhookCommand
 {
     /// <summary />
     public int OnExecute( CommandLineApplication app )